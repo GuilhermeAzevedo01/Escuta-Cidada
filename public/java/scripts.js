01067e7e58d472262edd87ac3d7d644f20dbdac3
Guilherme Azevedo <guilhermeruan3012@gmail.com>
2025-05-31 10:37:26 -0300

    add msg de erros

--- public/java/scripts.js
+++ public/java/scripts.js
@@ -51,6 +51,208 @@ document.addEventListener("DOMContentLoaded", function () {
       avsite: document.querySelector('input[name="avsite"]:checked')?.value || null,
     };
 
+
+     // Seleciona o elemento que exibe a mensagem de erro do campo "nome"
+      const erroNome = document.getElementById("erro-nome");
+
+      // Antes de validar, remove possíveis mensagens de erro antigas
+      nome.classList.remove("erro-input");         // Remove a borda vermelha, se existir
+      erroNome.style.display = "none";             // Esconde a mensagem de erro, se estiver visível
+
+      // Verifica se o campo "nome" está vazio
+      if (nome.value.trim() === "") {
+        e.preventDefault();                        // Impede o envio do formulário
+        nome.classList.add("erro-input");          // Adiciona a borda vermelha ao input
+        erroNome.style.display = "block";          // Exibe a mensagem de erro acima do campo
+        temErro = true;                            // Marca que há erro no formulário
+      }
+
+      // Quando o usuário começar a digitar no input "nome"
+      nome.addEventListener("input", function () {
+        nome.classList.remove("erro-input");       // Remove a borda vermelha
+        erroNome.style.display = "none";           // Esconde a mensagem de erro
+      });
+
+
+
+       const erroEmail = document.getElementById("erro-email");
+
+      // Antes de validar, remove possíveis mensagens de erro antigas
+      email.classList.remove("erro-input");         // Remove a borda vermelha, se existir
+      erroEmail.style.display = "none";             // Esconde a mensagem de erro, se estiver visível
+
+      // Verifica se o campo "nome" está vazio
+      if (email.value.trim() === "") {
+        e.preventDefault();                        // Impede o envio do formulário
+        email.classList.add("erro-input");          // Adiciona a borda vermelha ao input
+        erroEmail.style.display = "block";          // Exibe a mensagem de erro acima do campo
+        temErro = true;                            // Marca que há erro no formulário
+      }
+
+      // Quando o usuário começar a digitar no input "nome"
+      email.addEventListener("input", function () {
+      email.classList.remove("erro-input");       // Remove a borda vermelha
+      erroEmail.style.display = "none";           // Esconde a mensagem de erro
+      });
+
+
+
+      const erroGenero = document.getElementById("erro-genero");
+
+      // Antes de validar, remove possíveis mensagens de erro antigas
+      genero.classList.remove("erro-input");         // Remove a borda vermelha, se existir
+      erroGenero.style.display = "none";             // Esconde a mensagem de erro, se estiver visível
+
+      // Verifica se o campo "nome" está vazio
+      if (genero.value.trim() === "") {
+        e.preventDefault();                        // Impede o envio do formulário
+        genero.classList.add("erro-input");          // Adiciona a borda vermelha ao input
+        erroGenero.style.display = "block";          // Exibe a mensagem de erro acima do campo
+        temErro = true;                            // Marca que há erro no formulário
+      }
+
+      // Quando o usuário começar a digitar no input "nome"
+      genero.addEventListener("input", function () {
+      genero.classList.remove("erro-input");       // Remove a borda vermelha
+      erroGenero.style.display = "none";           // Esconde a mensagem de erro
+      });
+
+
+
+      const erroIdade = document.getElementById("erro-idade");
+
+      // Antes de validar, remove possíveis mensagens de erro antigas
+      idade.classList.remove("erro-input");         // Remove a borda vermelha, se existir
+      erroIdade.style.display = "none";             // Esconde a mensagem de erro, se estiver visível
+
+      // Verifica se o campo "nome" está vazio
+      if (idade.value.trim() === "") {
+        e.preventDefault();                        // Impede o envio do formulário
+        idade.classList.add("erro-input");          // Adiciona a borda vermelha ao input
+        erroIdade.style.display = "block";          // Exibe a mensagem de erro acima do campo
+        temErro = true;                            // Marca que há erro no formulário
+      }
+
+      // Quando o usuário começar a digitar no input "nome"
+      idade.addEventListener("input", function () {
+      idade.classList.remove("erro-input");       // Remove a borda vermelha
+      erroIdade.style.display = "none";           // Esconde a mensagem de erro
+      });
+
+
+
+      const erroTelefone = document.getElementById("erro-telefone");
+
+      // Antes de validar, remove possíveis mensagens de erro antigas
+      telefone.classList.remove("erro-input");         // Remove a borda vermelha, se existir
+      erroTelefone.style.display = "none";             // Esconde a mensagem de erro, se estiver visível
+
+      // Verifica se o campo "nome" está vazio
+      if (telefone.value.trim() === "") {
+        e.preventDefault();                        // Impede o envio do formulário
+        telefone.classList.add("erro-input");          // Adiciona a borda vermelha ao input
+        erroTelefone.style.display = "block";          // Exibe a mensagem de erro acima do campo
+        temErro = true;                            // Marca que há erro no formulário
+      }
+
+      // Quando o usuário começar a digitar no input "nome"
+      telefone.addEventListener("input", function () {
+      telefone.classList.remove("erro-input");       // Remove a borda vermelha
+      erroTelefone.style.display = "none";           // Esconde a mensagem de erro
+      });
+
+
+
+      const erroEmpresa = document.getElementById("erro-empresa");
+
+      // Antes de validar, remove possíveis mensagens de erro antigas
+      empresa.classList.remove("erro-input");         // Remove a borda vermelha, se existir
+      erroEmpresa.style.display = "none";             // Esconde a mensagem de erro, se estiver visível
+
+      // Verifica se o campo "nome" está vazio
+      if (empresa.value.trim() === "") {
+        e.preventDefault();                        // Impede o envio do formulário
+        empresa.classList.add("erro-input");          // Adiciona a borda vermelha ao input
+        erroEmpresa.style.display = "block";          // Exibe a mensagem de erro acima do campo
+        temErro = true;                            // Marca que há erro no formulário
+      }
+
+      // Quando o usuário começar a digitar no input "nome"
+      empresa.addEventListener("input", function () {
+      empresa.classList.remove("erro-input");       // Remove a borda vermelha
+      erroEmpresa.style.display = "none";           // Esconde a mensagem de erro
+      });
+
+
+
+    const erroLinha = document.getElementById("erro-linha");
+
+      // Antes de validar, remove possíveis mensagens de erro antigas
+      linha.classList.remove("erro-input");         // Remove a borda vermelha, se existir
+      erroLinha.style.display = "none";             // Esconde a mensagem de erro, se estiver visível
+
+      // Verifica se o campo "nome" está vazio
+      if (linha.value.trim() === "") {
+        e.preventDefault();                        // Impede o envio do formulário
+        linha.classList.add("erro-input");          // Adiciona a borda vermelha ao input
+        erroLinha.style.display = "block";          // Exibe a mensagem de erro acima do campo
+        temErro = true;                            // Marca que há erro no formulário
+      }
+
+      // Quando o usuário começar a digitar no input "nome"
+      linha.addEventListener("input", function () {
+      linha.classList.remove("erro-input");       // Remove a borda vermelha
+      erroLinha.style.display = "none";           // Esconde a mensagem de erro
+      });
+
+
+
+      const erroHorario = document.getElementById("erro-horario");
+
+      // Antes de validar, remove possíveis mensagens de erro antigas
+      horario.classList.remove("erro-input");         // Remove a borda vermelha, se existir
+      erroHorario.style.display = "none";             // Esconde a mensagem de erro, se estiver visível
+
+      // Verifica se o campo "nome" está vazio
+      if (horario.value.trim() === "") {
+        e.preventDefault();                        // Impede o envio do formulário
+        horario.classList.add("erro-input");          // Adiciona a borda vermelha ao input
+        erroHorario.style.display = "block";          // Exibe a mensagem de erro acima do campo
+        temErro = true;                            // Marca que há erro no formulário
+      }
+
+      // Quando o usuário começar a digitar no input "nome"
+      horario.addEventListener("input", function () {
+      horario.classList.remove("erro-input");       // Remove a borda vermelha
+      erroHorario.style.display = "none";           // Esconde a mensagem de erro
+      });
+
+
+
+      
+      const erroMensagem= document.getElementById("erro-mensagem");
+
+      // Antes de validar, remove possíveis mensagens de erro antigas
+      mensagem.classList.remove("erro-input");         // Remove a borda vermelha, se existir
+      erroMensagem.style.display = "none";             // Esconde a mensagem de erro, se estiver visível
+
+      // Verifica se o campo "nome" está vazio
+      if (mensagem.value.trim() === "") {
+        e.preventDefault();                        // Impede o envio do formulário
+        mensagem.classList.add("erro-input");          // Adiciona a borda vermelha ao input
+        erroMensagem.style.display = "block";          // Exibe a mensagem de erro acima do campo
+        temErro = true;                            // Marca que há erro no formulário
+      }
+
+      // Quando o usuário começar a digitar no input "nome"
+      mensagem.addEventListener("input", function () {
+      mensagem.classList.remove("erro-input");       // Remove a borda vermelha
+      erroMensagem.style.display = "none";           // Esconde a mensagem de erro
+      });
+
+
+
+
     try {
       // Envia os dados para a API via método POST, convertendo o objeto data para JSON
       const response = await fetch("https://escuta-cidada-back.onrender.com/api/feedback/", {
